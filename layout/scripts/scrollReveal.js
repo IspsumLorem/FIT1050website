f82ebe88b23f2de3684f2442c542864e9dcf24e7
TropicalHeights <sbha0043@student.monash.edu>
2021-05-24 13:12:43 +1000

    Added CSS and JS files

---
+++ layout/scripts/scrollReveal.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('#logo', {delay: 50});
+ScrollReveal().reveal('#top', {delay: 150});
+ScrollReveal().reveal('.clear', {delay: 50});
+ScrollReveal().reveal('.wh-trans', {delay: 125});
+ScrollReveal().reveal('video', {delay: 125});
+ScrollReveal().reveal('h2', {delay: 125});
+ScrollReveal().reveal('.group', {delay: 75});
+ScrollReveal().reveal('.btn', {delay: 75});
+ScrollReveal().reveal('.stubborn', {delay: 75});
+ScrollReveal().reveal('figure', {delay: 125});
+ScrollReveal().reveal('.btmspace-30', {delay: 125});
+ScrollReveal().reveal('.btmspace-15', {delay: 125});
+ScrollReveal().reveal('.overlay', {delay: 125});
+ScrollReveal().reveal('.inline clear', {delay: 125});
+ScrollReveal().reveal('.one_quarter first', {delay: 125});
+ScrollReveal().reveal('.one_quarter', {delay: 125});
+///These all target the individual elements. Some delays are made in order to 
+///make everything seem together
+
+/* should be noted that the code works in any order but I've tried my best to keep the .reveal() methods
+in the order the appear on the website */
